68a96de35ac5cac7002da48b290b1c50d2498a85
Pulkit <pulkitverma004@gmail.com>
2020-09-06 22:40:44 +0530

    Updates

---
+++ precache-manifest.b1b1b61bfa1423fc7283bdb1f5052245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3453d3f4517942a125d8869be283e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85905516f29020d2e29",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "59d83bcb4d7ea5bf7ead",
+    "url": "/static/css/main.30cab7ed.chunk.css"
+  },
+  {
+    "revision": "a85905516f29020d2e29",
+    "url": "/static/js/2.b8c801ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b8c801ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d83bcb4d7ea5bf7ead",
+    "url": "/static/js/main.bd6ee7f2.chunk.js"
+  },
+  {
+    "revision": "92e06d9dc1f120434c24",
+    "url": "/static/js/runtime-main.d5ee2753.js"
+  }
+]);
