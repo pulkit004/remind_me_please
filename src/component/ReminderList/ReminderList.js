a1a9286bb8dcdd28783de677d40bd1a7ac045c4f
Pulkit <pulkitverma004@gmail.com>
2020-09-06 17:38:32 +0530

    form added

--- src/component/ReminderList/ReminderList.js
+++ src/component/ReminderList/ReminderList.js
@@ -1,81 +1,90 @@
 import React, {Component} from 'react';
 import './ReminderList.css';
 import {Button, Input, Modal} from 'antd';
-import Locker from 'lockr'
+import Locker from 'lockr';
 import dayjs from 'dayjs';
-class ReminderList extends Component {
 
+class ReminderList extends Component {
 
   constructor(props) {
     super(props);
-    const reminderList = Locker.get('reminderList')
+    const reminderList = Locker.get('reminderList');
     this.state = {
-      visible: false,
-      item: '',
-      reminderList: reminderList || []
+      visible: false, item: '', reminderList: reminderList || [],
     };
   }
 
-  change = (event) => {
-    const value = event.target.value
+  change = (event, name) => {
+    const {state} = this;
+    state[name] = event.target.value;
     this.setState({
-      item: value
-    })
-  }
+      state,
+    });
+  };
 
   add = () => {
-      const {item, reminderList} = this.state;
-      const {selectedDate} = this.props
-      reminderList.unshift({item, selectedDate})
-      Locker.set('reminderList', reminderList)
-      this.setState({reminderList})
+    const {item, location, time, reminderList} = this.state;
+    const {selectedDate} = this.props;
+    reminderList.unshift({item, selectedDate, location, time});
+    Locker.set('reminderList', reminderList);
+    this.setState({reminderList});
   };
 
   render() {
-    const {visible, item, reminderList} = this.state;
+    const {visible, item, reminderList, location, time} = this.state;
 
     return (<div className='ReminderList'>
-          <Modal
-              title="Add new item to remember"
-              visible={visible}
-              centered
-              footer={null}
+      <Modal
+          title="Add new item to remember"
+          visible={visible}
+          centered
+          footer={null}
 
-              onCancel={() => this.setState({visible: false})}
-          >
-            <div className={'form-inline'}>
-              <Input name={'item'} value={item} onChange={this.change} placeholder="Meet Kunal at Bangalore HQ" />
-              <div className="px-1" />
-              <Button type={'primary'} onClick={this.add}>Add</Button>
-            </div>
-          </Modal>
-          <div className="header">
-            <div>
-              This saturday
-            </div>
-            <div>
+          onCancel={() => this.setState({visible: false})}
+      >
+        <div>
+          <span>Item</span>
+          <Input name={'item'} value={item} onChange={(e) => this.change(e, 'item')} placeholder="Meet Kunal at Bangalore HQ" />
+
+          <div className="my-2" />
+          <span>Location</span>
+
+          <Input name={'location'} value={location} onChange={(e) => this.change(e, 'location')} placeholder="Meet Kunal at Bangalore HQ" />
+          <div className="my-2" />
+
+          <span>Time</span>
+
+          <Input name={'time'} value={time} onChange={(e) => this.change(e, 'time')} placeholder="Meet Kunal at Bangalore HQ" />
+          <div className="my-2" />
+
+          <Button type={'primary'} onClick={this.add}>Add</Button>
+        </div>
+      </Modal>
+      <div className="header">
+        <div>
+          This saturday
+        </div>
+        <div>
                <span className={'cursor-pointer'} onClick={() => this.setState({visible: true})}>
                    Add new
                </span>
-            </div>
-          </div>
+        </div>
+      </div>
 
-          <div className="list-body">
-            <ul>
-              {
-                reminderList?.map(it => <li className={'mb-2'}>
-                 <p>
-                   {it?.item}
-                 </p>
-                  <p>
-                    {dayjs.unix(it?.selectedDate).format('DD MMM')}
+      <div className="list-body">
+        <ul>
+          {reminderList?.map(it => <li className={'mb-2'}>
+            <p>
+              {it?.item}
+            </p>
+            <p>
+              {dayjs.unix(it?.selectedDate).format('DD MMM')}
 
-                  </p>
-                </li> )
-              }
-            </ul>
-          </div>
-        </div>);
+            </p>
+          </li>)}
+        </ul>
+      </div>
+    </div>);
   }
 }
 
