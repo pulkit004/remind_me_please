88a56984a790063c86de9df55928cd41fc48cb28
Pulkit <pulkitverma004@gmail.com>
2020-09-06 22:35:52 +0530

    list is maintainded

--- src/component/ReminderList/ReminderList.js
+++ src/component/ReminderList/ReminderList.js
@@ -25,11 +25,19 @@ class ReminderList extends Component {
   add = () => {
     const {item, location, time, reminderList} = this.state;
     const {selectedDate} = this.props;
+    let reminderListItems = []
+    let computedObject = {}
 
     if(reminderList?.find(it => it.selectedDate === selectedDate)){
       let existingObject = reminderList?.find(it => it.selectedDate === selectedDate)
+      existingObject.items.unshift({item,location, time})
+
+    } else {
+      computedObject.selectedDate = selectedDate
+      reminderListItems.unshift({item, location, time})
+      computedObject.items = reminderListItems
+      reminderList.push(computedObject)
     }
-    reminderList.push({item, selectedDate, location, time});
     Locker.set('reminderList', reminderList);
     this.setState({reminderList});
   };
@@ -69,8 +77,14 @@ class ReminderList extends Component {
 
         </div>
         <div>
-               <span className={'cursor-pointer'} onClick={() => this.setState({visible: true})}>
-                   Add new
+               <span className={'cursor-pointer d-flex flex-row align-items-center  '} onClick={() => this.setState({visible: true})}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width="15" height="15" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" />
+  <line x1="12" y1="5" x2="12" y2="19" />
+  <line x1="5" y1="12" x2="19" y2="12" />
+</svg>
+                 <div className="px-1" />
+                 Add new
                </span>
         </div>
       </div>
@@ -80,18 +94,33 @@ class ReminderList extends Component {
           {reminderList?.sort((a,b) => b.selectedDate-a.selectedDate).map((it, index) => <li key={it?.name + `${index}`} className={'mb-2'}>
             <p className={'lead'}>
               {dayjs(it?.selectedDate).format('DD MMM')}
+            </p>
+            {
+              it?.items?.map(it => <div>
+                <p>
+                  {it?.name}
+                </p>
+                <p className={'d-flex flex-row align-items-center'}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width="15" height="15" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <circle cx="12" cy="11" r="3" />
+                    <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 0 1 -2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z" />
+                  </svg>
+                  <div className="px-1"/>
+                  {it?.location}
+                </p>
+                <p className={'d-flex flex-row align-items-center'}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width="15" height="15" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <circle cx="12" cy="12" r="9" />
+                    <polyline points="12 7 12 12 15 15" />
+                  </svg>
+                  <div className="px-1"/>
 
-            </p>
-            <p>
-              {it?.item}
-            </p>
-            <p>
-              {it?.location}
-            </p>
-
-            <p>
-              {it?.time}
-            </p>
+                  {it?.time}
+                </p>
+              </div>)
+            }
           </li>)}
         </ul>
       </div>
